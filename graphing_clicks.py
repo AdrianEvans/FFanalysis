a3b816261b2444a1337b4f2fe5d18814500d9fe8
AdrianEvans <aaevans@bradford.ac.uk>
2017-02-22 22:03:36 +0000

    Update graphing_clicks.py

--- graphing_clicks.py
+++ graphing_clicks.py
@@ -3,12 +3,11 @@
 import matplotlib.pyplot as plt
 import MySQLdb
 
-
 #database connection
 conn = MySQLdb.connect("localhost","adrianevans","CJ3ByCTc!","fossilfinder")
 c = conn.cursor()
 
-subject_of_interest = 1145456
+subject_of_interest = 940440
 
 #extracts x y and labels into rows
 c.execute("select x_coord from click_data_t3 where subject_ids = %s",(subject_of_interest,))
@@ -53,21 +52,26 @@ for i in l:
 
 
 
-
 for mar, col, lab, _x, _y in zip(marker_shape, marker_colour, l, x, y):
     plt.scatter(_x, _y, label=lab, c=col, marker=mar, alpha=.5)
     
 plt.axis([0.0,795,0.0,545])
+im = plt.imread("940440.png")
+implot = plt.imshow(im)
 ax = plt.gca()
 ax.set_autoscale_on(False)
 ax.invert_yaxis()
 ax.set_aspect('equal')
+ax.axis('off')
+ax.set_frame_on(False)
+plt.tight_layout()
 #ax.legend()
 
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title(subject_of_interest)
+#plt.xlabel('x')
+#plt.ylabel('y')
+#plt.title(subject_of_interest)
 #plt.show()
 
+
 file_name = str(subject_of_interest) + "_clicks.eps"
-plt.savefig(file_name,)
+plt.savefig(file_name, bbox_inches=0, pad_inches=0)
